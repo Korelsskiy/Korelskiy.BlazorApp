ebf523ff7d5b3975dbd6649afe306fed9b741d16
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-10 17:43:32 +0300

    Добавьте файлы проекта.

---
+++ Data/DB.cs
@@ -0,0 +1,16 @@
+﻿using Korelskiy.BlazorApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Korelskiy.BlazorApp.Data
+{
+    public class DB : DbContext
+    {
+        public DB(DbContextOptions<DB> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Korelskiy.BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Korelskiy.BlazorApp.Data.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<TodoItem> GetAllItems();
+        void AddTodo(string todoName);
+        void ValueChanged(TodoItem changedItem);
+        void DeleteItem(int id);
+    }
+}
